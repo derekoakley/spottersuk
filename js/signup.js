d63b0b238b89ea7d7a8bf94d4b2741197fca1179
Derek Oakley <derek@oakley.design>
2017-04-07 09:59:36 +0100

    INITIAL

---
+++ js/signup.js
@@ -0,0 +1,1 @@
+var signup=function(){var t=document.getElementsByTagName("main")[0],e=document.querySelector("[data-signup=form]"),n=function(t){"scrollTo"===t.target.dataset.signup&&e.scrollIntoView({behavior:"smooth"})};!function(){t.addEventListener("click",n)}()}();
